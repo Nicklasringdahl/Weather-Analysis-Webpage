466867577a5d93456d61a95800349e76f197a0a8
Nicklas Ringdahl <Nicklas.ringdahl@gmail.com>
2021-10-27 15:47:33 +1100

    finally fixed relative paths

--- WebVisualisation/Pandas_to_html.py
+++ WebVisualisation/Pandas_to_html.py
@@ -3,7 +3,3 @@ import pandas as pd
 # create dataframe
 df = pd.read_csv("C:\Users\Nick\Documents\School\Homework\Web-Design-Challenge\WebVisualisation\Resources\cities.csv")
 df.to_html('Cities.html')
-
-# render dataframe as html
-html = df.to_html()
-print(html)
