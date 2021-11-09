cf956d355b754d13bbf833ed9f2b093913cb44f6
Nicklas Ringdahl <Nicklas.ringdahl@gmail.com>
2021-11-09 19:08:46 +1100

    added finishing touches

--- WebVisualisation/Pandas_to_html.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-# create dataframe
-df = pd.read_csv("C:\Users\Nick\Documents\School\Homework\Web-Design-Challenge\WebVisualisation\Resources\cities.csv")
-df.to_html('Cities.html')
